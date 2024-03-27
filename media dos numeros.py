08fcab3e5f7c5820e8039e5c9165cb0d402b8e41
Ulisses Genguini <42587034+ulissesg@users.noreply.github.com>
2024-03-27 08:30:04 -0300

    Add files via upload

---
+++ media dos numeros.py
@@ -0,0 +1,15 @@
+print("======================")
+print("Resolução da média    ")
+print("======================")
+
+n1 = float(input("Digite o primeiro numero"))
+n2 = float(input("Digite o segundo numero"))
+n3 = float(input("Digite o terceiro numero"))
+n4 = float(input("Digite o quarto numero"))
+
+m = ((n1+n2+n3+n4))
+print("A soma é (",m,")")
+
+d= (m/4)
+print("A média é (",d,")")
+    
